183a70f4c42b83bf608eb8ed810ff1d004ceb340
Silas P Ladislau <splhead@gmail.com>
2021-04-28 16:29:09 -0400

    148 - primeiros testes

---
+++ client/src/components/Logo/test.tsx
@@ -0,0 +1,21 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import Logo from '.'
+
+describe('<Logo />', () => {
+  it('should render a black label by default', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('should render a white label when color is passed', () => {
+    renderWithTheme(<Logo color="white" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+})
