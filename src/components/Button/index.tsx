4bdfdd852edbdb9d5b73a1a018321c07d1aab105
Silas P Ladislau <silas.ladislau@ale.ro.gov.br>
2021-05-06 15:51:26 -0400

    170 - Button as link

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { ButtonHTMLAttributes } from 'react'
+import { AnchorHTMLAttributes } from 'react'
 import * as S from './styles'
 
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>
+
 export type ButtonProps = {
-  children?: React.ReactNode
+  // children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
   icon?: JSX.Element
-  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
-}
+  as?: React.ElementType
+  // onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
+} & ButtonTypes
 
 const Button = ({
   children,
