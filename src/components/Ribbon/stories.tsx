75cdb4970283e6550380bfc5f1f3cffcff3b394f
Silas P Ladislau <silas.ladislau@ale.ro.gov.br>
2021-05-07 17:05:59 -0400

    177 - Banner + Ribbon

---
+++ src/components/Ribbon/stories.tsx
@@ -0,0 +1,36 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Ribbon, { RibbonProps } from '.'
+
+export default {
+  title: 'Ribbon',
+  component: Ribbon,
+  args: {
+    children: 'Best Seller'
+  },
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    color: {
+      options: ['primary', 'secondary'],
+      defaultValue: 'primary'
+    },
+    size: {
+      options: ['normal', 'small'],
+      defaultValue: 'normal'
+    }
+  }
+} as Meta
+
+export const Default: Story<RibbonProps> = (args) => (
+  <div
+    style={{
+      width: '40rem',
+      height: '25rem',
+      position: 'relative',
+      backgroundColor: '#888'
+    }}
+  >
+    <Ribbon {...args} />
+  </div>
+)
