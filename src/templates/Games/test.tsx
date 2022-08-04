b7231c06c7e7c3657ac4aafe966fa73f21fd6592
Silas P Ladislau <splhead@gmail.com>
2022-08-04 12:43:47 -0400

    refactor: :poop: improve games templates and test

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -1,13 +1,24 @@
 import { screen } from '@testing-library/react'
 import { MockedProvider } from '@apollo/client/testing'
-import { renderWithTheme } from 'utils/tests/helpers'
 
-import filterMock from 'components/ExploreSidebar/mock'
+import { renderWithTheme } from 'utils/tests/helpers'
+import filterItemsMock from 'components/ExploreSidebar/mock'
+import { fetchMoreMock, gamesMock } from './mocks'
 
 import Games from '.'
-import { fetchMoreMock, gamesMock } from './mocks'
 import userEvent from '@testing-library/user-event'
 import apolloCache from 'utils/apolloCache'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+const push = jest.fn()
+
+useRouter.mockImplementation(() => ({
+  push,
+  query: '',
+  asPath: '',
+  route: '/'
+}))
 
 jest.mock('templates/Base', () => ({
   __esModule: true,
@@ -16,10 +27,10 @@ jest.mock('templates/Base', () => ({
   }
 }))
 
-jest.mock('components/ExploreSidebar', () => ({
+jest.mock('next/link', () => ({
   __esModule: true,
-  default: function Mock() {
-    return <div data-testid="Mock ExploreSidebar" />
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div>{children}</div>
   }
 }))
 
@@ -27,42 +38,64 @@ describe('<Games />', () => {
   it('should render loading when starting the template', () => {
     renderWithTheme(
       <MockedProvider mocks={[]} addTypename={false}>
-        <Games filterItems={filterMock} />
+        <Games filterItems={filterItemsMock} />
       </MockedProvider>
     )
 
     expect(screen.getByText(/loading.../i)).toBeInTheDocument()
   })
 
-  it('should render the sections', async () => {
+  it('should render sections', async () => {
     renderWithTheme(
       <MockedProvider mocks={[gamesMock]} addTypename={false}>
-        <Games filterItems={filterMock} />
+        <Games filterItems={filterItemsMock} />
       </MockedProvider>
     )
 
+    // it starts without data
+    // shows loading
     expect(screen.getByText(/loading.../i)).toBeInTheDocument()
 
-    expect(await screen.findByTestId('Mock ExploreSidebar')).toBeInTheDocument()
-
+    // we wait until we have data to get the elements
+    // get => tem certeza do elemento
+    // query => Não tem o elemento
+    // find => processos assincronos
+    expect(await screen.findByText(/Price/i)).toBeInTheDocument()
     expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
 
     expect(
-      screen.getByRole('button', { name: /show more/i })
+      await screen.findByRole('button', { name: /show more/i })
     ).toBeInTheDocument()
   })
 
   it('should render more games when show more is clicked', async () => {
     renderWithTheme(
       <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
-        <Games filterItems={filterMock} />
+        <Games filterItems={filterItemsMock} />
       </MockedProvider>
     )
 
     expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
 
-    userEvent.click(screen.getByRole('button', { name: /show more/i }))
+    userEvent.click(await screen.findByRole('button', { name: /show more/i }))
 
     expect(await screen.findByText(/Fetch More Game/i)).toBeInTheDocument()
+  })
+
+  it('should change push router when selecting a filter', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+
+    userEvent.click(await screen.findByRole('checkbox', { name: /windows/i }))
+    userEvent.click(await screen.findByRole('checkbox', { name: /linux/i }))
+    userEvent.click(await screen.findByLabelText(/low to high/i))
+
+    expect(push).toHaveBeenCalledWith({
+      pathname: '/games',
+      query: { platforms: ['windows', 'linux'], sort_by: 'low-to-high' }
+    })
   })
 })
